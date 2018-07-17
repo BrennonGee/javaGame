67d85638f4dc2b693e6c712f0dfde6ae0dcbcc2b
Unknown <reagangee@gmail.com>
2018-07-17 12:52:10 -0400

    //JPanel

--- window.java
+++ window.java
@@ -6,10 +6,15 @@ import java.awt.*;
 
 public class window {
     public static void start(){
-      JFrame frame = new JFrame();
-      frame.setSize( new Dimension (500, 400));
-      frame.setLocationRelativeTo(null);
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setVisible(true);
+        JFrame frame = new JFrame();
+        
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.black);
+        frame.getContentPane().add(panel);
+        frame.setSize( new Dimension (500, 400));
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
